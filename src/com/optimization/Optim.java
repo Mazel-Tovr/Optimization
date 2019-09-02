479bdf4448d550145b0a61dbbef82653d3d40829
Egor <ze99@rambler.ru>
2019-09-02 20:38:24 +0400

    jdk 11.0

---
+++ src/com/optimization/Optim.java
@@ -0,0 +1,75 @@
+package com.optimization;
+
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+public class Optim {
+    private final double a;
+    private final double b;
+    private final double epsilon;
+
+    Optim(double a, double b, double epsilon) {
+        this.a = a;
+        this.b = b;
+        this.epsilon = epsilon;
+    }
+
+    private double Function(double z) {
+        return Math.pow(z, 4.0D) + Math.pow(z, 2.0D) + z + 1.0D;
+    }
+
+    private double DerivativeFunction(double z) {
+        return 4.0D * Math.pow(z, 3.0D) + 2.0D * z + 1.0D;
+    }
+
+    public void GetDihord() {
+        double x1 = this.a;
+        double x2 = this.b;
+
+        do {
+            double xn = (x1 + x2) / 2.0D;
+            if (this.Function(xn) > 0.0D) {
+                x2 = xn;
+            } else {
+                x1 = xn;
+            }
+
+            System.out.println(xn);
+        } while(Math.abs(x1 - x2) > this.epsilon);
+
+    }
+
+    public void GetHord() {
+        double xn = this.a;
+        double fb = this.Function(this.b);
+        double fxn = this.Function(xn);
+
+        double prevxn;
+        do {
+            prevxn = xn;
+            System.out.println(xn);
+            xn = this.b - (this.b - xn) * fb / (fb - fxn);
+            fxn = this.Function(xn);
+            System.out.println(xn);
+        } while(Math.abs(xn - prevxn) > this.epsilon);
+
+    }
+
+    public void GetKasatelbnblx() {
+        double xn = this.a;
+        double fxn = this.Function(xn);
+        double Defxn = this.DerivativeFunction(xn);
+
+        double prevxn;
+        do {
+            prevxn = xn;
+            xn -= fxn / Defxn;
+            fxn = this.Function(xn);
+            Defxn = this.DerivativeFunction(xn);
+            System.out.println(xn);
+        } while(Math.abs(xn - prevxn) > this.epsilon);
+
+    }
+}

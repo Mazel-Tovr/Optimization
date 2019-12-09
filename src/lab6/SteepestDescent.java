fa97d50b14464ea7bfbd9a45cfc005e3b93a24c1
Егор <egorzavgorodnev@gmail.com>
2019-12-09 17:54:42 +0300

    add first method lab 6 java

--- src/lab6/SteepestDescent.java
+++ src/lab6/SteepestDescent.java
@@ -1,19 +1,101 @@
 package lab6;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SteepestDescent
 {
+    private int NUMBER_OF_ITERATIONS = 100000;
+    private double EPS = 1e-5;
 
-    void search()
-    {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Метод наискорейщего спуска");
-        System.out.println("Функция Z=F(X1,X2,...,XN) вычисляется в строке 2000");
-        System.out.println("Частные производные G(1),G(2),...G(N) Вычисляются в строке 3000");
-        System.out.println("Введите число переменных");
-        int N = in.nextInt() + 1;
-        double[] X,Y,G,D = new double[N];
-        double[] L,FF = new double[4];
+    List<Double> goldenSectionOptimize(List<Double> x,double a,double b, int n){
+        List<Double> tmp = x;
+        double s1;
+        double s2;
+        double u1;
+        double u2;
+        double fu1;
+        double fu2;
+
+        List<Double> GF=GradientF(x);
+
+        s1 = (3-Math.sqrt(5D))/2;
+        s2 = (Math.sqrt(5D-1)/2;
+        u1 = a+s1*(b-a);
+        u2 = a+s2*(b-a);
+
+        for(int j=0;j<x.size();j++)
+            tmp.set(j,x.get(j)+u1*GF.get(j));
+        fu1 = f(tmp);
+
+        for(int j=0;j<x.size();j++)
+            tmp.set(j,x.get(j)+u2*GF.get(j));
+        fu2 = f(tmp);
+
+        for(int i = 1; i <= n; i++)
+        {
+            if( fu1<=fu2 )
+            {
+                b = u2;
+                u2 = u1;
+                fu2 = fu1;
+                u1 = a+s1*(b-a);
+                for(int j=0;j<x.size();j++)
+                    tmp.set(j,x.get(j)+u1*GF.get(j));
+                fu1 = f(tmp);
+            }
+            else
+            {
+                a = u1;
+                u1 = u2;
+                fu1 = fu2;
+                u2 = a+s2*(b-a);
+                for(int j=0;j<x.size();j++)
+                    tmp.set(j,x.get(j)+u2*GF.get(j));
+                fu2 = f(tmp);
+            }
+        }
+        for(int j=0;j<x.size();j++)
+            tmp.set(j,x.get(j)+u1*GF.get(j));
+        fu1 = f(tmp);
+        for(int j=0;j<x.size();j++)
+            tmp.set(j,x.get(j)+u2*GF.get(j));
+        fu2 = f(tmp);
+
+        if (fu1<fu2)
+            for(int j=0;j<x.size();j++)
+                tmp.set(j,x.get(j)+u1*GF.get(j));
+        else
+            for(int j=0;j<x.size();j++)
+                tmp.set(j,x.get(j)+u2*GF.get(j));
+
+        return tmp;
     }
+
+    double f (List<Double> x) {
+        return 10*x.get(0)*x.get(0)+x.get(1)*x.get(1);
+    }
+
+    List<Double> GradientF(List<Double> x) {
+        List<Double> tmp = new ArrayList<>() {};
+        //tmp.add(-2*(1-x.get(0)) - 400 *(x.get(1)-x.get(0)*x.get(0))*x.get(0));
+        //tmp.add(200*x.get(1));
+        tmp.add(20*x.get(0));
+        tmp.add(2*x.get(1));
+        return tmp;
+    }
+
+    List<Double> GradientDescent(int N,List<Double> x0) {
+        List<Double> old = new ArrayList<Double>(), cur_x = x0, gr;
+
+        for (int Iterations=1; Iterations<=NUMBER_OF_ITERATIONS; Iterations++) {
+            //save old value
+            old = cur_x;
+            //compute gradient
+            gr = GradientF(cur_x);
+            cur_x=goldenSectionOptimize(cur_x,-10,10,100);
+        }
+    }
+
+
 }
